5fbb0f722bf0ae4907f2a16018b4df11fa3d6536
DeCastelino <daryllcastelino007@gmail.com>
2021-06-18 08:08:03 +1000

    added files via upload

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const connection = require('./model/database')
 const { connect } = require('./model/database')
 const AWS = require('aws-sdk')
 const path = require('path')
+const WebSocket = require('ws')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const SpotifyWebApi = require('spotify-web-api-node')
 
 // Load config file
 dotenv.config({ path: './config/.env' })
@@ -12,6 +16,9 @@ dotenv.config({ path: './config/.env' })
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //takes the url encoded data and passes that into an oject we can use with the req when doing form data stuff 
 app.use(express.urlencoded({ extended: true }));
@@ -31,15 +38,15 @@ const dynamoDB = new AWS.DynamoDB();
 
 // root will rediect to login page
 app.get('/', (req, res) => {
-    res.redirect('login')
+    res.redirect('app-login')
 })
 
-app.get('/login', (req, res) => {
+app.get('/app-login', (req, res) => {
     res.render('login.ejs', { error: false })
 })
 
 // checking login credentials
-app.post('/login', async (req,res) => {
+app.post('/app-login', async (req, res) => {
     const query = 'SELECT FirstName FROM Users WHERE `Email` = ? AND `Password` = ?'
     const values = [req.body.email, req.body.password]
     await connection.query(query, values, (error, results, fields) => {
@@ -47,7 +54,7 @@ app.post('/login', async (req,res) => {
         if (error) {
             console.error('Error while fetching data from database: ' + error.stack);
         }
-        if(results != '') {
+        if (results != '') {
             console.log('Fetching data successful');
             res.redirect('dashboard')
         }
@@ -58,7 +65,7 @@ app.post('/login', async (req,res) => {
 })
 
 // render register page
-app.get('/register', (req,res) => {
+app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
@@ -71,23 +78,16 @@ app.post('/register', async (req, res) => {
         }
         else {
             console.log('User data entered successfully!');
-            res.redirect('login')
+            res.redirect('app-login')
         }
     })
 })
 
 app.get('/dashboard', (req, res) => {
-    
+
     // setting parameters to retreive all posts available
     var params = {
-        TableName: 'user-post',
-        // FilterExpression: '#title = :title',
-        // ExpressionAttributeNames: {
-        //     '#title': 'title',
-        // },
-        // ExpressionAttributeValues: {
-        //     ':title': 'title'
-        // }
+        TableName: 'user-post'
     }
 
     docClient.scan(params, (err, data) => {
@@ -105,7 +105,7 @@ app.get('/dashboard', (req, res) => {
 app.post('/addPost', (req, res) => {
     // setting parameters to add post to database
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' ('+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds()+')';
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' (' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds() + ')';
     var params = {
         TableName: 'user-post',
         Item: {
@@ -125,6 +125,66 @@ app.post('/addPost', (req, res) => {
             res.redirect('dashboard')   // redirecting to dashboard to see newly added post as well
         }
     })
+})
+
+app.get('/chat', (req, res) => {
+    const wsURL = process.env.WS_URL
+    const ws = new WebSocket(wsURL)     // connecting to web socket
+    res.render('chat.ejs', { messages: '' })
+
+})
+
+
+//----------Spotify Routes-----------
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken,
+    })
+
+    spotifyApi
+        .refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+})
+
+app.post("/login", (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+    })
+
+    spotifyApi
+        .authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            })
+        })
+        .catch(err => {
+            res.sendStatus(400)
+        })
+})
+
+app.get("/lyrics", async (req, res) => {
+    const lyrics =
+        (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+    res.json({ lyrics })
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
